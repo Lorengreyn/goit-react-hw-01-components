4d65c4aac8a2040e15388246ac4980347ce55862
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-05-29 16:04:46 +0300

    3.2

--- src/components/Friends/FriendsLi.jsx
+++ src/components/Friends/FriendsLi.jsx
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types';
-import FriendsListItem from './FriendsListItem';
 import css from './Friends.module.css';
 
 function FriendsLi({ friends }) {
   return (
     <ul className={css.itemList}>
       {friends.map(({ id, name, isOnline, avatar }) => (
-        <FriendsListItem
-          key={id}
-          name={name}
-          isOnline={isOnline}
-          avatar={avatar}
-        />
+        <li className={css.item}
+        key={id}
+        name={name}
+        avatar={avatar}>
+      <span className={isOnline ? css.statusOnline : css.statusOffline}></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.friendsName}>{name}</p>
+    </li>
+        
       ))}
     </ul>
   );
