7a0dc5144164631344abdeb55b1bdf17065fcd48
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-05-29 17:41:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import friends from './components/Friends/friends.json';
 import TransactionHistory from './components/Transactions/TransactionHistory';
 import transactions from './components/Transactions/transaction.json';
 
-import BrowserRouter from 'react-dom';
+
 
 function App() {
   const { tag, username, location, avatar, stats } = user;
@@ -35,9 +35,6 @@ function App() {
       <Statistics title="Upload stats" stats={data} />
       <FriendsLi friends={friends} />
       <TransactionHistory items={transactions} />
-      <BrowserRouter basename="/goit-react-hw-01-components/">
-  <App />
-</BrowserRouter>
     </div>
   );
 }
